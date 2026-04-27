32474bcd4516b6483a04dc458969955b7126206a
snackpei <snackpei974@gmail.com>
2026-04-27 21:43:16 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,25 @@
-
-const CACHE_NAME = "samoussapp-v1";
+const CACHE_NAME = "samoussapp-v2";
 
 const FILES_TO_CACHE = [
-  "index.html",
-  "bar.html",
-  "carte.html",
-  "categorie.html",
-  "ajouter.html",
-  "admin.html",
-  "manifest.json"
+  "./",
+  "index.html?v=2",
+  "bar.html?v=2",
+  "carte.html?v=2",
+  "categorie.html?v=2",
+  "ajouter.html?v=2",
+  "login.html?v=2",
+  "dashboard.html?v=2",
+  "devenir-samoussatier.html?v=2",
+  "connexion-samoussatier.html?v=2",
+  "dashboard-samoussatier.html?v=2",
+  "manifest.json",
+  "icon-192.png?v=2",
+  "icon-512.png?v=2"
 ];
 
 self.addEventListener("install", function(event){
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache){
       return cache.addAll(FILES_TO_CACHE);
@@ -19,10 +27,36 @@ self.addEventListener("install", function(event){
   );
 });
 
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.map(function(key){
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(function(){
+      return self.clients.claim();
+    })
+  );
+});
+
 self.addEventListener("fetch", function(event){
   event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(function(response){
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(function(cache){
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(function(){
+        return caches.match(event.request);
+      })
   );
 });
